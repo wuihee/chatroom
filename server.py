b80df9a32c830d7baefdddb67fe635c67e28c904
wuihee <wuihee@gmail.com>
2024-01-19 20:59:15 -0800

    Updated code

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import selectors
 import socket
 import sys
+import types
 
 sel = selectors.DefaultSelector()
 
@@ -14,6 +15,34 @@ lsock.listen()
 print(f"Listening on {(host, port)}")
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb="b", outb="b")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            data.out += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)
+            data.outb = data.outb[sent:]
+
 
 try:
     while True:
