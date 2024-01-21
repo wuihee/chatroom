f69d05b91e3beaf5464228ecc42732c04d17741b
wuihee <wuihee@gmail.com>
2024-01-21 01:39:14 -0800

    Updated server code

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import selectors
 import socket
 
 HOST = socket.gethostbyname(socket.gethostname())
@@ -6,9 +7,32 @@ PORT = 1234
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 server_socket.listen()
+server_socket.setblocking(False)
 
-client_socket, address = server_socket.accept()
-print(f"Accepted connection from {address[0]}.")
+selector = selectors.DefaultSelector()
+selector.register(server_socket, selectors.EVENT_READ)
 
-message = client_socket.recv(1024)
-client_socket.send(f"{message.decode()}".encode("utf-8"))
+
+def accept_connection(server_socket: socket.socket) -> None:
+    """
+    Accept a new connection from a new client.
+
+    Args:
+        server_socket (socket.socket): Socket object for the server.
+    """
+    client_socket, address = server_socket.accept()
+    print(f"Accepted connection from {address[0]}.")
+    client_socket.setblocking(False)
+    selector.register(client_socket, selectors.EVENT_READ)
+
+
+def service_connection():
+    pass
+
+
+while True:
+    client_socket, address = server_socket.accept()
+    print(f"Accepted connection from {address[0]}.")
+
+    message = client_socket.recv(1024)
+    client_socket.send(f"{message.decode()}".encode("utf-8"))
