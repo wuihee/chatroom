4d5427b897895a6b7c97a859d8190f8de132c211
wuihee <wuihee@gmail.com>
2024-01-25 19:32:12 -0800

    Updated server and client

--- chatroom/client.py
+++ chatroom/client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 1234
@@ -7,9 +8,36 @@ HEADER_SIZE = 64
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-while True:
-    message = input("> ")
+
+def receive_message(client_socket: socket.socket) -> None:
+    """
+    Receive a message from the server.
+
+    Args:
+        client_socket (socket.socket): Client socket to receive message from.
+    """
+    while True:
+        message_length = int(client_socket.recv(HEADER_SIZE).decode("utf-8"))
+        print(client_socket.recv(message_length).decode("utf-8"))
+
+
+def send_message(client_socket: socket.socket, message: str) -> None:
+    """
+    Send a message through a socket.
+
+    Args:
+        client_socket (socket.socket): Socket to send message through.
+        message (str): Message to send.
+    """
     message_length = len(message)
     header = f"{message_length:>{HEADER_SIZE}}"
     client_socket.send(header.encode("utf-8"))
     client_socket.send(message.encode("utf-8"))
+
+
+username = input("Enter your name: ")
+threading.Thread(target=receive_message, args=(client_socket,)).start()
+
+while True:
+    message = input("")
+    send_message(client_socket, f"[{username}]: {message}")
--- chatroom/server.py
+++ chatroom/server.py
@@ -12,6 +12,7 @@ server_socket.setblocking(False)
 
 selector = selectors.DefaultSelector()
 selector.register(server_socket, selectors.EVENT_READ)
+active_clients = []
 
 
 def accept_connection(server_socket: socket.socket) -> None:
@@ -25,11 +26,26 @@ def accept_connection(server_socket: socket.socket) -> None:
     print(f"Accepted connection from {address[0]}.")
     client_socket.setblocking(False)
     selector.register(client_socket, selectors.EVENT_READ)
+    active_clients.append(client_socket)
+
+
+def broadcast(message: str) -> None:
+    """
+    Broadcast message to all active clients.
+
+    Args:
+        message (str): Message to broadcast.
+    """
+    for client_socket in active_clients:
+        message_length = len(message)
+        header = f"{message_length:>{HEADER_SIZE}}"
+        client_socket.send(header.encode("utf-8"))
+        client_socket.send(message.encode("utf-8"))
 
 
 def receive_message(key: selectors.SelectorKey, mask: int) -> None:
     """
-    Recieve a message from an existing connection.
+    Receive a message from an existing connection.
 
     Args:
         key (selectors.SelectorKey): The ready socket object.
@@ -39,7 +55,7 @@ def receive_message(key: selectors.SelectorKey, mask: int) -> None:
     if mask & selectors.EVENT_READ:
         message_length = int(client_socket.recv(HEADER_SIZE).decode("utf-8"))
         message = client_socket.recv(message_length).decode("utf-8")
-        print(f"[{client_socket.getsockname()[0]}]: {message}")
+        broadcast(message)
 
 
 print(f"Server started on {HOST} on port {PORT}.")
